512c44eb69774c8b660d7cdf1841f23f2f877d0e
Marshall Sosland <marshall.sosland@gmail.com>
2016-02-25 14:08:47 -0600

    take form city/state to output parks object

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,27 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#start').on('submit', function(e) {
+    e.preventDefault();
+    var coords = $(this).serialize();
+    $.ajax({
+      url: "https://maps.googleapis.com/maps/api/geocode/json?" + coords + "&key=KEY_here",
+      type: "GET"
+    }).done(function(response) {
+      var lat = response["results"][0]["geometry"]["location"]["lat"];
+      var lon = response["results"][0]["geometry"]["location"]["lng"]
+      $.ajax({
+      url: "https://trailapi-trailapi.p.mashape.com/?limit=25&radius=200&lat=" + lat + "&lon=" + lon,
+      type: "GET",
+      xhrFields:
+      {
+        withCredentials: true
+      },
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('X-Mashape-Key', 'key_here');
+      }}).done(function(response) {
+        console.log(response);
+      });
+    });
+  });
 });
